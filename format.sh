b0330a024447c52a56b2cd6a4cdcdf46afce527a
HiroIshida <h-ishida@jsk.imi.i.u-tokyo.ac.jp>
2025-04-06 20:24:36 +0900

    Move main to node_script

--- format.sh
+++ format.sh
@@ -1,3 +1,3 @@
 find . -name "*py"|xargs python3 -m autoflake -i --remove-all-unused-imports --remove-unused-variables --ignore-init-module-imports
-python3 -m isort *.py --profile black
-python3 -m black --line-length 100 --target-version py38 --required-version 22.6.0 *.py
+python3 -m isort node_script/*.py --profile black
+python3 -m black --line-length 100 --target-version py38 --required-version 22.6.0 node_script/*.py
