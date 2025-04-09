acd85ee7f0a06aadf545f02edbb76ad75f725672
HiroIshida <h-ishida@jsk.imi.i.u-tokyo.ac.jp>
2025-04-09 14:40:59 +0900

    Send q_goal = q_leader + (q_leader - q_follower) * coef

--- node_script/follower_teleop.py
+++ node_script/follower_teleop.py
@@ -4,12 +4,15 @@ from node_utils import FollowJointTrajectoryClient, JointStateProvider
 
 
 def main_loop():
-    js_provider = JointStateProvider(namespace=None)
+    js_provider_leader = JointStateProvider(namespace=None)
+    js_provider_follower = JointStateProvider(namespace="follower")
     fjt_client = FollowJointTrajectoryClient(namespace="follower")
     while not rospy.is_shutdown():
-        joint_state = js_provider.joint_angles
-        fjt_client.send_goal(joint_state, duration=0.1, blocking=False)
-        rospy.sleep(0.02)
+        q_leader = js_provider_leader.joint_angles
+        q_follower = js_provider_follower.joint_angles
+        q_goal = q_leader + (q_leader - q_follower) * 1.0
+        fjt_client.send_goal(q_goal, duration=0.5, blocking=False)
+        rospy.sleep(0.05)
 
 
 if __name__ == "__main__":
